d71581b7c935c84774616dd03a0db242909c5e02
DESKTOP-DGLA2MI\UNFGRUPPE3PC2 <prog@gr3.gdc>
2018-07-27 22:13:06 +0200

    Now with double jumps! (and minor changes)

---
+++ Spil/Assets/Scripts/IsStanding.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsStanding : MonoBehaviour {
+    public bool debugging = true;
+
+    public bool onGround = false;
+    public PlayerMovement playermovement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag.ToLower() == "ground")
+        {
+            playermovement.grounded = true;
+            playermovement.canAirjump = true;
+        }
+        else if (collision.gameObject.tag.ToLower() == "water" && debugging)
+        {
+            playermovement.transform.Translate(new Vector3(0, 30, 0));
+        }
+    }
+
+    private void OnTriggerExit(Collider bye)
+    {
+        if(bye.gameObject.tag.ToLower() == "ground") playermovement.grounded = false;
+    }
+}
---
+++ Spil/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float movspd = 1337;
+    public float jumpForce = 10;
+    public float secondJumpMultiplier = 0.9f;
+    public int PlayerID;
+    public string lastDirection = "none";
+
+    public bool grounded = true;
+    public bool canAirjump = true;
+
+    // Use this for initialization
+    void Start () {
+        if (Input.GetAxis("Horizontal" + PlayerID) > 0) lastDirection = "right";
+        else lastDirection = "left";
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump" + PlayerID))
+        {
+            //First jump
+            if (grounded) Jump();
+            //Double jump
+            else if (canAirjump)
+            {
+                Jump();
+                canAirjump = false;
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y * secondJumpMultiplier, rigidBody.velocity.z);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move(movspd * Input.GetAxis("Horizontal"+PlayerID));
+    }
+
+    
+    void Move(float speed)
+    {
+        rigidBody.velocity = (Vector3.up * rigidBody.velocity.y) + (transform.right * speed);
+    }
+
+    void Jump()
+    {
+        grounded = false;
+        rigidBody.velocity = new Vector3 (rigidBody.velocity.x, 0 , rigidBody.velocity.z);
+        rigidBody.AddForce(jumpForce * transform.up, ForceMode.Impulse);
+    }
+}
