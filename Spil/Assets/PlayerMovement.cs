30edbeab845624eebe259d5eeac1871cfb8b72c7
DESKTOP-DGLA2MI\UNFGRUPPE3PC2 <prog@gr3.gdc>
2018-07-27 19:10:47 +0200

    Basic test map og movement mechanics lavet

---
+++ Spil/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+    public float movspd = 1337;
+    public float jumpForce = 10;
+    public int PlayerID;
+    // Use this for initialization
+    /*void Start () {
+		
+	}*/
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump" + PlayerID))
+        {
+            jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move(movspd * Input.GetAxis("Horizontal"+PlayerID));
+    }
+
+
+
+    void Move(float speed)
+    {
+        rigidBody.velocity = (Vector3.up * rigidBody.velocity.y) + (transform.right * speed);
+    }
+
+    void jump()
+    {
+        rigidBody.AddForce(jumpForce * transform.up, ForceMode.Impulse);
+    }
+}
